555b2598a55db9bbbcf3ade275e599cf4449bf04
Nedu Robert <nedumrobert@gmail.com>
2019-03-12 10:52:12 +0100

    Feat: User actions: login, register, logout

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -3,14 +3,20 @@ import { userService } from '../services';
 import { alertActions } from './';
 import { history } from '../helpers';
 
-export const userActions = {
-    login,
-    logout,
-    register
-};
-
 function login(username, password) {
     // return the promise using fetch which adds to localstorage on resolve
+    return dispatch => {
+        dispatch(request({ username, password }));
+
+        userService.login(username, password)
+        .then(user => {
+            dispatch(success(user));
+            history.push('/');
+        }, error => {
+            dispatch(failure(error));
+            dispatch(alertActions.error(error));
+        })
+    }
 
     function request(user) { return { type: userConstants.LOGIN_REQUEST, user } }
     function success(user) { return { type: userConstants.LOGIN_SUCCESS, user } }
@@ -19,12 +25,36 @@ function login(username, password) {
 
 function logout() {
     // complete this function
+    userService.logout();
+    return { type: userConstants.LOGOUT };
 }
 
 function register(user) {
     // return the promise using fetch which dispatches appropriately 
+    return dispatch => {
+        dispatch(request(user));
+
+        userService.register(user)
+            .then(
+                user => { 
+                    dispatch(success(user));
+                    history.push('/login');
+                    dispatch(alertActions.success('Registration successful'));
+                },
+                error => {
+                    dispatch(failure(error));
+                    dispatch(alertActions.error(error));
+                }
+            );
+    };
 
     function request(user) { return { type: userConstants.REGISTER_REQUEST, user } }
     function success(user) { return { type: userConstants.REGISTER_SUCCESS, user } }
     function failure(error) { return { type: userConstants.REGISTER_FAILURE, error } }
 }
+
+export const userActions = {
+    login,
+    logout,
+    register
+};
