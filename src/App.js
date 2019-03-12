2f1a742eb18d930cf8c08164da5228bfe6fbd959
Nedu Robert <nedumrobert@gmail.com>
2019-03-12 10:54:51 +0100

    Feat: Alert messages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import { Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+
 import { PrivateRoute } from './PrivateRoute.js';
 import { history } from './helpers';
 import { alertActions } from './actions';
 import { HomePage } from './components/HomePage';
-import { LoginPage } from './components/LoginPage';
-import { RegisterPage } from './components/RegisterPage';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
 
-export class App extends React.Component {
+class App extends React.Component {
     constructor(props) {
         super(props);
 
         const { dispatch } = this.props;
         history.listen((location, action) => {
+            dispatch(alertActions.clear());
         });
     }
 
     render() {
         const { alert } = this.props;
         return (
-              <div className="container">
-                  <div className="col-sm-8 col-sm-offset-2">
+            <div className="container">
+                <div className="col-sm-8 col-sm-offset-2">
                     {
                         alert && alert.message ? <div className = {`alert ${alert.type}`}> {alert.message} </div> : null
                     }
@@ -32,15 +34,19 @@ export class App extends React.Component {
                         <Route path="/register" component={RegisterPage} />
                     </div>
                     </Router>
-                  </div>
-              </div>
+                </div>
+            </div>
         );
     }
 }
 
 function mapStateToProps(state) {
-    const { alert } = state;
+    const { alert, authentication } = state;
     return {
-        alert
+        alert,
+        authentication
     };
 }
+
+const connectApp = connect(mapStateToProps)(App);
+export { connectApp as App }; 
