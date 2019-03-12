ba9971bacb806a59709c725ead3448ac16cc3109
Nedu Robert <nedumrobert@gmail.com>
2019-03-12 11:02:05 +0100

    Feat: Register page

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 
 import { userActions } from '../actions';
 
-export class RegisterPage extends Component {
+class RegisterPage extends Component {
     constructor(props) {
         super(props);
 
@@ -23,21 +23,37 @@ export class RegisterPage extends Component {
 
     handleChange(event) {
         // handle input change and dispatch register
+        const { name, value } = event.target;
+        const { user } =  this.state;
+        this.setState({ 
+            user: {
+                ...user,
+                [name]: value 
+            }
+        });
     }
 
     handleSubmit(event) {
         // handle button click and dispatch register
+        event.preventDefault();
+        const { user } = this.state;
+        const { dispatch } = this.props;
+        if (user.username && user.password) {
+            dispatch(userActions.register(user));
+        }
+        this.setState({ ...this.state, submitted: true });
     }
 
     render() {
+        const { registering } = this.props;
         const { user, submitted } = this.state;
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h2>Register</h2>
-                <form name="form">
+                <form name="form" onSubmit={this.handleSubmit}>
                     <div className={'form-group' + (submitted && !user.username ? ' has-error' : '')}>
                         <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control username" name="username" />
+                        <input type="text" className="form-control username" value={user.username} onChange={this.handleChange} name="username" />
                         {submitted && !user.username &&
                             <div className="help-block">Username is required</div>
                         }
@@ -51,6 +67,9 @@ export class RegisterPage extends Component {
                     </div>
                     <div className="form-group">
                         <button className="btn btn-primary">Register</button>
+                        {registering &&
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif" width="13%"  />
+                        }
                         <Link to="/login" className="btn btn-link">Cancel</Link>
                     </div>
                 </form>
@@ -61,7 +80,11 @@ export class RegisterPage extends Component {
 
 // complete the below function
 function mapStateToProps(state) {
-    
+    const { registration: { registering } } = state;
+    return {
+        registering
+    };
 }
 
+export default connect(mapStateToProps)(RegisterPage);
 export { RegisterPage as TestRegisterPage };
